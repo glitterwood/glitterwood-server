e6c28fcd1033a2c3725f6bce7ce33141289757c2
Dave Edelhart <dave@wonderlandlabs.com>
2015-07-23 10:07:31 -0700

    work on avatar images

--- models/players.js
+++ models/players.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 var schema = {
   name: {type: 'string', index: {unique: true}},
-  rank: {type: 'number', defaultValue: 1200}
+  rank: {type: 'number', defaultValue: 1200},
+  avatar: {type: 'buffer' }
 };
 
 var Players = mongoose.model('players', schema);
--- routers/players.js
+++ routers/players.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var Players = require('./../models/players');
+var multer = require('multer');
+var util = require('util');
 
 // define the home page route
 router.get('/', function (req, res) {
   Players.find({}, function (err, players) {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    console.log('returning ', players.length, 'players');
+    players = players.map(function (p) {
+      var out = p.toJSON();
+      if (p.avatar) {
+        out.avatar = p.avatar.toString();
+      }
+      return out;
+    });
     res.send(players);
   });
 });
@@ -12,8 +25,8 @@ router.get('/', function (req, res) {
 router.delete('/:id?',
   function (req, res) {
     var id = req.params.id;
-    if (id == 'all'){
-      Players.collection.remove(function(){
+    if (id == 'all') {
+      Players.collection.remove(function () {
         res.send({removed: 'all'});
       })
     } else {
@@ -21,11 +34,13 @@ router.delete('/:id?',
     }
   });
 
-router.post('/', function (req, res) {
+router.post('/', multer().single('avatar-file'), function (req, res) {
   var data = req.body;
   if (typeof data == 'string') {
     data = JSON.parse(data);
   }
+
+  util.log(util.inspect(req));
 
   Players.find({name: data.name}, function (err, result) {
     if (err) {
@@ -33,6 +48,7 @@ router.post('/', function (req, res) {
     } else if (result.length) {
       res.status(409).send({error: 'name already exists', name: data.name});
     } else {
+      console.log('req.body:', req.body);
       new Players(data).save(function (err, newRecord) {
         if (err) {
           res.status(500).send(err);
@@ -41,7 +57,8 @@ router.post('/', function (req, res) {
         }
       });
     }
-  })
+  });
+
 });
 
 module.exports = router;
