a203197ece79fb56b3ae33cfea9b6c131eb04d71
Dave Edelhart <dave@wonderlandlabs.com>
2015-07-22 20:18:31 -0700

    moving cors up

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ var cors = require('cors');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
 app.use(cors());
-
+app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
