1834d86b07503da8ba3771c7314db5ec21cf5f5f
Dave Edelhart <dave@wonderlandlabs.com>
2015-07-22 20:16:11 -0700

    adding CORS

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 
+var cors = require('cors');
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(cors());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
